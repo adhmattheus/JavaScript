5c2f576055524b18a8d00df5bad10631bc64c5ca
adhmattheus <mattheusadhonnaysf@hotmail.com>
2021-07-21 11:08:15 -0300

    finished game 100 %

--- popballoonsgame.js
+++ popballoonsgame.js
@@ -3,6 +3,8 @@ function startGame() {
     window.location.href = 'popballoonsgame2.html?' + level_game;
 }
 
+var timeId = null; //armazena a chamda da funcao timeout
+
 function inciaJogo() {
 
     var url = window.location.search;
@@ -27,12 +29,32 @@ function inciaJogo() {
 
 
     // quantidade de baloes
-    var qtde_baloes = 15;
+    var qtde_baloes = 5;
     cria_baloes(qtde_baloes);
 
     //imprimir qted_baloes
     document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
     document.getElementById('baloes_estourados').innerHTML = 0;
+
+    contagem_tempo(tempo_segundos + 1);
+}
+
+function contagem_tempo(segundos) {
+
+    segundos = segundos - 1;
+    if (segundos == -1) {
+        clearTimeout(timeId); // para a execucao da funcao do settimeout;
+        game_over();
+        return false;
+    }
+
+    document.getElementById('cronometro').innerHTML = segundos;
+    timeId = setTimeout("contagem_tempo(" + segundos + ")", 1000);
+}
+
+function game_over() {
+    remove_eventos_baloes();
+    alert('GAME OVER');
 }
 
 function cria_baloes(qtde_baloes) {
@@ -42,7 +64,60 @@ function cria_baloes(qtde_baloes) {
         var balao = document.createElement("img");
         balao.src = 'imagens/balao_azul_pequeno.png';
         balao.style.margin = '10px';
+        balao.id = 'b' + i; // id de cada balao
+        balao.onclick = function () { estourar(this); };
 
         document.getElementById('cenario').appendChild(balao);
+    }
+}
+
+function estourar(e) {
+
+    var id_balao = e.id;
+
+    document.getElementById(id_balao).setAttribute("onclick", "");
+
+    document.getElementById(id_balao).src = 'imagens/balao_azul_pequeno_estourado.png';
+
+    pontuacao(-1);
+}
+
+function pontuacao(acao) {
+
+    var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
+    var baloes_estourados = document.getElementById('baloes_estourados').innerHTML;
+
+    baloes_inteiros = parseInt(baloes_inteiros);
+    baloes_estourados = parseInt(baloes_estourados);
+
+    baloes_inteiros = baloes_inteiros + acao;
+    baloes_estourados = baloes_estourados - acao;
+
+    document.getElementById('baloes_inteiros').innerHTML = baloes_inteiros;
+    document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
+
+    status_jogo(baloes_inteiros);
+}
+
+function status_jogo(baloes_inteiros) {
+
+    if (baloes_inteiros == 0) {
+        alert('parabéns, vc conseguiu !');
+        stop_game();
+    }
+}
+
+function stop_game() {
+    clearTimeout(timeId);
+}
+
+function remove_eventos_baloes() {
+    var i = 1; //contado para recuperar balões por id
+
+    //percorre o lementos de acordo com o id e só irá sair do laço quando não houver correspondência com elemento
+    while (document.getElementById('b' + i)) {
+        //retira o evento onclick do elemnto
+        document.getElementById('b' + i).onclick = '';
+        i++; //faz a iteração da variávei i
     }
 }
